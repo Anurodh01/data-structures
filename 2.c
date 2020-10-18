7eda0d7df605f4b736be884ad40378de2722a8d0
Anurodh01 <67429000+Anurodh01@users.noreply.github.com>
2020-10-18 09:47:11 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float c,f,d;
+    printf("enter the temperature in celcius: ");
+    scanf("%f",&c);
+    f=9.0/5.0*c+32;
+    printf("temp in fahrenheit is=%f",f);
+    return 0;
+
+}
